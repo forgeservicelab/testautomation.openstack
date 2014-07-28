392b69b58e7958efa2c83fffbd17225c40d45209
antti.koskinen <antti.koskinen@forgeservicelab.fi>
2014-07-28 12:27:44 +0300

    Documentation block to test cases added

--- Functions.py
+++ Functions.py
@@ -1,2 +1,11 @@
 def size(arr):
         return len(arr)
+
+def Compare_Ignore_Case(s1, s2):
+    if s1.lower() != s2.lower():
+        return True
+    else:
+        return False
+    
+def To_First_Upper(s1):
+    return s1[:1].upper() + s1[1:].lower()
